020856ab9264c1b32e4ff15d7680f73db3c75ff8
ABissPro <109984434+ABissPro@users.noreply.github.com>
2025-02-23 16:46:29 +0300

    Update 3. Использование assert.cpp

--- 3. Использование assert.cpp
+++ 3. Использование assert.cpp
@@ -1,32 +1,32 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cassert>
 
-float travelTime(float distance, float runnersS[], float time[]) {
-    for (int j = 0; j < 5; j++) {
-        time[j] = distance / runnersS[j];
-    }
-    return 0;
+float travelTime(float distance, float speed) {
+    assert(speed > 0);
+    return distance / speed;
 }
 
 int main()
 {
     system("chcp 1251"); // Установить кодировку
     float distance;
-    float runnersS[5] = {};
-    float time[5] = {};
+    float speed;
+    float runnersS[5];
+
     std::cout << "Введите дистанцию: ";
     std::cin >> distance;
     assert(distance > 0);
-    
+
     for (int i = 0; i < 5; i++) {
         std::cout << "Введите скорость:\n";
-        std::cin >> runnersS[i];
+        std::cin >> speed;
+        runnersS[i] = speed;
         assert(runnersS[i] > 0);
     }
 
-    travelTime(distance, runnersS, time);
+    
     for (int i = 0; i < 5; i++) {
-        std::cout << "Бегун №" << i + 1 << ": " << time[i] << " секунд.\n";
+        float time = travelTime(distance, speed);
+        std::cout << "Бегун №" << i + 1 << ": " << time << " секунд.\n";
     }
 }
-
